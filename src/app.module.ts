ae05dceacaa905fed5b3d13c12ed71f25527f2cf
awais512 <raza.awais25@gmail.com>
2021-01-21 17:13:39 +0500

    Environment Variable setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ResturantsModule } from './resturants/resturants.module';
@@ -9,6 +10,10 @@ import { ResturantsModule } from './resturants/resturants.module';
       autoSchemaFile: true,
     }),
     ResturantsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.dev.env' : '.test.env ',
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
