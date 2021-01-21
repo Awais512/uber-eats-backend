9cf9bb8313baf60c82ba9573991a56296b493df9
awais512 <raza.awais25@gmail.com>
2021-01-21 19:30:59 +0500

    Validate Config Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ResturantsModule } from './resturants/resturants.module';
+import * as joi from 'joi';
 
 @Module({
   imports: [
@@ -13,6 +14,15 @@ import { ResturantsModule } from './resturants/resturants.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'dev' ? '.dev.env' : '.test.env ',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: joi.object({
+        NODE_ENV: joi.string().valid('dev', 'prod'),
+        DB_HOST: joi.string().required(),
+        DB_PORT: joi.string().required(),
+        DB_USERNAME: joi.string().required(),
+        DB_PASSWORD: joi.string().required(),
+        DB_DATABASE: joi.string().required(),
+      }),
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
